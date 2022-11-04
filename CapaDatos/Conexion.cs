f7908489db472b48b5b666195f5640449a7c6c6d
USUARIO <USUARIO@DESKTOP-RCVV74R>
2022-11-04 00:09:16 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+       // public static string CN = "Data Source=DBSistemaVentas.mssql.somee.com;Initial Catalog=DBSistemaVentas;user id=juanpgil_SQLLogin_1;pwd=vg2mt52z62";
+    }
+}
